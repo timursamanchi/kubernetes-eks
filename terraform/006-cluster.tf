adb3d46c1989211e3a45a89b37ccc00350638431
timursamanchi <timur.samanchi@gmail.com>
2025-07-31 13:47:12 +0100

    adding 005-iam.tf

---
+++ terraform/006-cluster.tf
@@ -0,0 +1,65 @@
+################################################################################
+# 006-EKS-CLUSTER.TF
+# ------------------------------------------------------------------------------
+# Provisions an EKS cluster using EC2 worker nodes (managed node group)
+# IAM roles are auto-managed by the EKS module
+# Includes core add-ons (CoreDNS, kube-proxy, VPC CNI) and CloudWatch log group
+################################################################################
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "${var.project_name}-eks"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+
+  enable_irsa               = true
+  create_iam_role           = true
+  create_node_iam_role      = true
+  cluster_security_group_id = module.eks_security_group.security_group_id
+
+  eks_managed_node_groups = {
+    quoteapp_nodes = {
+      ami_id         = "ami-01f23391a59163da9"  # Replace with your custom Ubuntu AMI ID
+      instance_types = ["t3.small"]
+      disk_size      = 30
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+
+      labels = {
+        role = "worker"
+        app  = "quoteapp"
+      }
+
+      tags = {
+        Name = "quoteapp-node"
+      }
+    }
+  }
+
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
+
+  create_cloudwatch_log_group             = true
+  cloudwatch_log_group_retention_in_days = 7
+
+  tags = {
+    Project   = var.project_name
+    Terraform = "true"
+  }
+}
--- terraform/100-outputs.tf
+++ terraform/100-outputs.tf
@@ -1,49 +1,49 @@
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
+# output "vpc_id" {
+#   value = module.vpc.vpc_id
+# }
 
-output "public_subnets" {
-  value = module.vpc.public_subnets
-}
+# output "public_subnets" {
+#   value = module.vpc.public_subnets
+# }
 
-output "private_subnets" {
-  value = module.vpc.private_subnets
-}
+# output "private_subnets" {
+#   value = module.vpc.private_subnets
+# }
 
-output "nat_gateway_ids" {
-  value = module.vpc.natgw_ids
-}
+# output "nat_gateway_ids" {
+#   value = module.vpc.natgw_ids
+# }
 
-output "availability_zones" {
-  value = local.selected_azs
-}
-output "public_subnet_cidrs" {
-  value = local.public_subnet_cidrs
-}
+# output "availability_zones" {
+#   value = local.selected_azs
+# }
+# output "public_subnet_cidrs" {
+#   value = local.public_subnet_cidrs
+# }
 
-output "private_subnet_cidrs" {
-  value = local.private_subnet_cidrs
-}
-output "eks_cluster_name" {
-  value = module.eks.cluster_name
-}
+# output "private_subnet_cidrs" {
+#   value = local.private_subnet_cidrs
+# }
+# output "eks_cluster_name" {
+#   value = module.eks.cluster_name
+# }
 
-output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
+# output "eks_cluster_endpoint" {
+#   value = module.eks.cluster_endpoint
+# }
 
-output "eks_oidc_provider_arn" {
-  description = "OIDC provider ARN for IRSA"
-  value       = module.eks.oidc_provider_arn
-}
+# output "eks_oidc_provider_arn" {
+#   description = "OIDC provider ARN for IRSA"
+#   value       = module.eks.oidc_provider_arn
+# }
 
-output "eks_cluster_security_group_id" {
-  value = module.eks.cluster_security_group_id
-}
-output "quote_frontend_log_group" {
-  value = aws_cloudwatch_log_group.quote_frontend.name
-}
+# output "eks_cluster_security_group_id" {
+#   value = module.eks.cluster_security_group_id
+# }
+# output "quote_frontend_log_group" {
+#   value = aws_cloudwatch_log_group.quote_frontend.name
+# }
 
-output "quote_backend_log_group" {
-  value = aws_cloudwatch_log_group.quote_backend.name
-}
+# output "quote_backend_log_group" {
+#   value = aws_cloudwatch_log_group.quote_backend.name
+# }
