4e44fb2b598b5a65086ce59bc419d90bf1f11d4d
timursamanchi <147069682+timursamanchi@users.noreply.github.com>
2025-07-31 23:52:45 +0100

    removed unwanted code

--- terraform/002-main.tf
+++ terraform/002-main.tf
@@ -1,21 +1,3 @@
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-#   token                  = data.aws_eks_cluster_auth.cluster.token
-# }
-
-# data "aws_eks_cluster" "cluster" {
-#   name = module.eks.cluster_name
-
-#   depends_on = [module.eks]  # 👈 this is key
-# }
-
-# data "aws_eks_cluster_auth" "cluster" {
-#   name = module.eks.cluster_name
-
-#   depends_on = [module.eks]  # 👈 this too
-# }
-
 #######################################
 # Get available AZs
 #######################################
